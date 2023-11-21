9c2792002a013d132e2b8b58038e18da7dec216d
Joseph Bedford <jibbystar@proton.me>
2023-11-21 20:30:42 +0000

    Stylistic change

--- .xscripts/commands.sh
+++ .xscripts/commands.sh
@@ -4,13 +4,13 @@
 #
 
 echo '(
-	# command												bg	kill
-	"nitrogen --set-scaled $HOME/.background.jpg"			1	""
-	"picom"													1	"picom"
-	"setxkbmap -layout gb"									1	""
-	"dunst"													1	"dunst"
-	"( sleep 1 && notify-send \"Logged in.\" )"				1	""
-	"$HOME/.xscripts/status.sh"								1	"status.sh"
-	"( sudo ntpd -qg && sudo hwclock -w )"					1	""
+	# command, bg, kill
+	"nitrogen --set-scaled $HOME/.background.jpg" 1 ""
+	"picom" 1 "picom"
+	"setxkbmap -layout gb" 1 ""
+	"dunst" 1 "dunst"
+	"( sleep 1 && notify-send \"Logged in.\" )" 1 ""
+	"$HOME/.xscripts/status.sh" 1 "status.sh"
+	"( sudo ntpd -qg && sudo hwclock -w )" 1 ""
 )'
 
